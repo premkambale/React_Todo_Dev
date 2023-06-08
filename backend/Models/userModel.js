2bc2e95919835804cf2f515bb5839e0358239492
Coder-SagarThakare <saggythakare01@gmail.com>
2023-06-08 11:07:00 +0530

    bcrypt added

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -2,15 +2,22 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name: {
+    userName: {
         type: String,
         required: true
     },
-
     password: {
         type: String,
         required: true
-    }
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    mobileNo: {
+        type: Number,
+        required: true
+    },
 })
 
-module.exports = mongoose.model('/user', userSchema);
+module.exports = mongoose.model('user', userSchema);
--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const userDB = require('../Models/userModel')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 
 router.post('/', async (req, res) => {
+    async function getHashedPassword(password) {
+        const salt = await bcrypt.genSalt(10);
+        return await bcrypt.hash(password, salt);
+    }
 
     const newUser = new userDB({
-        name: 'sagar',
-        password: 'pass-sagar'
+        userName: req.body.userName,
+        password: await getHashedPassword(req.body.password),
+        confirmPassword: req.body.confirmPassword,
+        email: req.body.email,
+        mobileNo: req.body.mobileNo
     })
 
     try {
