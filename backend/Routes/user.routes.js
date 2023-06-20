92fd1df0ead420b6684e065d8ad06d2fa7560410
Coder-SagarThakare <saggythakare01@gmail.com>
2023-06-21 01:31:33 +0530

    boiler plate structure added for task

--- backend/Routes/user.routes.js
+++ backend/Routes/user.routes.js
@@ -3,24 +3,15 @@ const router = express.Router();
 const verifyToken = require("../Middlewares/verifyToken");
 const { userController } = require("../Controllers");
 // const authMiddleware = require('../Middlewares/authMiddleware.js')
-
+const taskRoutes = require('../Routes/task.routes')
 router.all("/*", verifyToken);
 
-// get profile data
-// router.get("/profile", userController.getProfile);
-
-//update the profile
-// router.patch("/profile", userController.updateProfile);
-
-// delete profile
-// router.delete("/profile", userController.deleteProfile);
+router.use('/task',taskRoutes)
 
 router
   .route("/profile")
   .get(userController.getProfile)
   .delete(userController.deleteProfile);
 
-router.route("/profile/:id").patch(userController.updateProfile);
-// router.route("/profile").get(userController.getProfile).patch(userController.updateProfile).delete(userController.deleteProfile);
 
 module.exports = router;
